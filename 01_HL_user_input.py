25f066266301ecca67b4b4886481fc269297be1d
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2020-02-28 10:56:02 +1300

    Current

---
+++ 01_HL_user_input.py
@@ -0,0 +1,16 @@
+# HL - Get (and Check) user input
+
+# To Do
+# Check the lowest is an integer (any integer)
+# Check that the highest is more than the lowest (lower bound only)
+# Check that rounds is more than 1 (upper bound only)
+# Check that the guess is between lowest and highest (lower and upper bound)
+
+# Number Checking function
+def intcheck(question, low, high):
+
+
+# Main Routine
+
+lowest = intcheck("Low Number: ")
+highest = intcheck("High Number: ", lowest + 1)
